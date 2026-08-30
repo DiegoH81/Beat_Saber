9ec5f0534116aca7a5fa7729e2922bf6cf41d0c0
GebUCSP <jose.cornejo.castro@ucsp.edu.pe>
2026-08-30 16:37:31 -0500

    feat(enemy): create base enemy

---
+++ enemies/enemy.gd
@@ -0,0 +1,19 @@
+extends Area3D
+class_name Enemy
+
+
+@export_category("Attributes")
+@export var max_velocity: float = 0.0
+@export var acceleration: float = 0.0
+@export var direction: Vector3 = Vector3.ZERO
+
+var velocity: Vector3 = Vector3.ZERO
+
+func _ready() -> void:
+	assert(max_velocity != 0, "[%s]: max velocity is zero" % name)
+	assert(acceleration != 0, "[%s]: acceleration is zero" % name)
+	assert(direction != Vector3.ZERO, "[%s]: direction is zero" % name)
+	
+func _physics_process(delta: float) -> void:
+	velocity = velocity.move_toward(max_velocity * direction, acceleration * delta)
+	position += velocity * delta
