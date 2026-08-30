3c68c473dba19d84239bd3512ff25a4eb7fe35ec
GebUCSP <jose.cornejo.castro@ucsp.edu.pe>
2026-08-30 18:05:28 -0500

    refactor(generator): expert system WIP

--- src/enemies/generator/generator.gd
+++ src/enemies/generator/generator.gd
@@ -5,13 +5,34 @@ class_name Generator
 
 @export var points: Array[Node3D]
 
+var memory: int = -1
+
 var max_intervals: float = 2
 var intervals: float = 2
 
 func _process(delta: float) -> void:
 	if intervals < 0:
 		intervals = max_intervals
-		var random_value: int = randi_range(0, points.size() - 1)
-		points[random_value].add_child(test_enemy.instantiate())
+		
+		var index: int
+		if memory != -1:
+		
+			if memory == 2:
+				index = 3
+			elif memory == 3:
+				index = 2
+				
+			elif memory == 6:
+				index = 7
+			elif memory == 7:
+				index = 6
+
+			memory = -1
+		else:
+			index = randi_range(0, points.size() - 1)
+			memory = index
+		points[index].add_child(test_enemy.instantiate())
+		
+		print(index)
 	
 	intervals -= delta
