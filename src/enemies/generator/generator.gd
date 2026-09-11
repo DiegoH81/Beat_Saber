b37ed367e862505d7da1deb07091cb1ad95f7d36
GebUCSP <jose.cornejo.castro@ucsp.edu.pe>
2026-09-10 23:11:05 -0500

    feat(generator): add intensity generation for enemies

--- src/enemies/generator/generator.gd
+++ src/enemies/generator/generator.gd
@@ -13,8 +13,13 @@ func _process(delta: float) -> void:
 		intervals = max_intervals
 		
 		var index: int = randi_range(0, points.size() - 1)
-		points[index].add_child(test_enemy.instantiate())
+		var intensity: float = randf_range(0.5,3)
 		
-		print(index)
+		var enemy: Node3D = test_enemy.instantiate()
+		enemy.scale = Vector3(intensity,intensity,intensity)
+		points[index].add_child(enemy)
+		
+		print(intensity)
+		print("Enemigo en la posicion %d con intensidad %.3f" % [index, intensity])
 	
 	intervals -= delta
