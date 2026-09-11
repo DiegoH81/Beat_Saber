98f5c2f2deabc21307d668147fc1df9c203f0bbd
GebUCSP <jose.cornejo.castro@ucsp.edu.pe>
2026-09-11 11:45:15 -0500

    feat(visual): change shader wip

--- levels_data/scenes/test.gd
+++ levels_data/scenes/test.gd
@@ -3,8 +3,12 @@ extends Node3D
 var xr_interface: XRInterface
 
 func _ready():
-	AudioManager.play_music("test")
+	#AudioManager.play_music("test")
+	VisualManager.register_world_env($Ambient)
+	pass
 
 func _unhandled_input(event: InputEvent) -> void:
 	if event.is_action_pressed("Escape"):
 		get_tree().quit()
+	if event.is_action_pressed("p_button"):
+		VisualManager.change_shader("toon_effect")
--- src/managers/visual_manager.gd
+++ src/managers/visual_manager.gd
@@ -3,16 +3,24 @@ extends Node
 
 var player: Node = null
 var generator: Node = null
+var world_env: WorldEnvironment
+
+var shader_path: String = "res://assets/shaders/"
 
 func register_player(node: Node) -> void:
-	if not player:
+	if not node:
 		return
 	player = node
 
 func register_generator(node: Node) -> void:
-	if not generator:
+	if not node:
 		return
 	generator = node
+
+func register_world_env(node: Node) -> void:
+	if not node:
+		return
+	world_env = node
 
 func change_theme(id) -> void:
 	change_shader(id)
@@ -20,7 +28,19 @@ func change_theme(id) -> void:
 	change_enemy_style(id) 
 
 func change_shader(id: String) -> void:
-	pass
+	if not world_env:
+		return
+	
+	var shader: CompositorEffect = load(shader_path + id + ".gd").new()
+	if not shader:
+		return
+	
+	print("Cargando: ", shader_path + id + ".gd")
+	print("Shader cargado: ", shader)
+	
+	world_env.compositor.compositor_effects.clear()
+	world_env.compositor.compositor_effects.push_back(shader)
+	
 
 func change_weapon_style(id: String) -> void:
 	pass
