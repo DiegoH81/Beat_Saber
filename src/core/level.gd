95e48bfcbf64824cda6de83bfc7cecba30b7c4c5
GebUCSP <jose.cornejo.castro@ucsp.edu.pe>
2026-09-25 11:41:39 -0500

    feat(level): add level1 ambient split

--- src/core/level.gd
+++ src/core/level.gd
@@ -3,6 +3,31 @@ class_name Level
 
 @export var player_vr: XROrigin3D
 @export var generator: Generator
+@export var ambient: WorldEnvironment
+
+var number: int = 0
 
 func _enter_tree() -> void:
 	AudioManager.play_music(name)
+	VisualManager.register_world_env(ambient)
+
+func _unhandled_input(event: InputEvent) -> void:
+	if event.is_action_pressed("p_button"):
+		if number == 0:
+			VisualManager.change_shader("classic")
+			number=1
+		elif number == 1:
+			VisualManager.change_shader("toon")
+			number=2
+		elif number == 2:
+			VisualManager.change_shader("new_mill")
+			number=3
+		elif number == 3:
+			VisualManager.change_shader("new_dec")
+			number=0
+	
+	if event.is_action_pressed("f_button"):
+		if Input.mouse_mode == Input.MOUSE_MODE_CAPTURED:
+			Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
+		else:
+			Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
